6d4bd1a83ccf23ab8d880fe581cffd4f9285dd58
Shubham <shubham.prasad10101@gmail.com>
2022-05-14 22:35:41 +0530

    added dark mode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 function App(){
+    const [mode, setMode] = useState('light')
+    const [alert, setAlert] = useState(null)
+
+    const showAlert= function (message,type)
+    {
+        setAlert({
+            msg: message,
+            type: type
+        })
+        
+        setTimeout(()=>{
+            setAlert(null)
+        },1500)
+    }
+     const changeMode = ()=>{
+         if(mode==='light')
+         {
+            setMode('dark')
+            document.body.style.backgroundColor="rgb(16, 38, 74)";
+            document.body.style.color="white";
+            document.title='TextUtils - dark mode'
+            showAlert("Dark mode is enabled", "success")
+         }
+         else
+         {
+            setMode('light')
+            document.body.style.backgroundColor="white";
+            document.body.style.color="black";
+            document.title='TextUtils - light mode'
+            showAlert("Dark mode is disabled", "success")
+         }
+     }
 
     return(
         <>
-            <Navbar title='TextUtils' about="About TextUtils"/>
+            <Navbar title='TextUtils' about="About TextUtils" mode={mode} changeMode={changeMode}/>
+            <Alert alert={alert} mode={mode}/>
             <br />
             <div className='container'>
-                <TextForm heading="Enter Text to be manipulated"/>
+                <TextForm heading="Enter Text to be manipulated" showAlert={showAlert} mode={mode}/>
             </div>
             {/* <About/> */}
         </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,34 +7,39 @@ export default function TextForm(props) {
         setText(text.toLowerCase())
         const arr = text.split(" ")
         for (var i = 0; i < arr.length; i++) {
-            arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-
+            arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1).toLowerCase();
         }
         const str = arr.join(" ");
         setText(str);
+    }
+
+    function handleClear(){
+        props.showAlert("Cleared the text","success")
+        setText("")
     }
     return (
         <>
             <div className='container'>
                 <h2>{props.heading}</h2>
                 <div className="mb-3">
-                    <textarea onChange={event=>setText(event.target.value)} className="form-control" aria-label="With textarea" rows='8' value={text}></textarea>
+                    <textarea onChange={event=>setText(event.target.value)} style={{backgroundColor: props.mode==='dark'?'rgb(168, 173, 181)':'white', color: 'white'}} className="form-control" aria-label="With textarea" rows='8' value={text}></textarea>
                 </div>
-                <button onClick={()=>setText(text.toUpperCase())} className='btn btn-primary'>Convert to uppercase</button>
-                <button onClick={()=>setText(text.toLowerCase())} className='btn btn-primary'>Convert to lowercase</button>
-                <button onClick={capitalize} className='btn btn-primary'>Capitalize</button>
-                <button onClick={()=>setText("")} className='btn btn-primary'>Clear Text</button>
+                <button onClick={()=>setText(text.toUpperCase())} className={`btn btn-${props.mode==='light'?'dark':'light'}`}>Convert to uppercase</button>
+                <button onClick={()=>setText(text.toLowerCase())} className={`btn btn-${props.mode==='light'?'dark':'light'}`}>Convert to lowercase</button>
+                <button onClick={capitalize} className={`btn btn-${props.mode==='light'?'dark':'light'}`}>Capitalize</button>
+                <button onClick={handleClear} className={`btn btn-${props.mode==='light'?'dark':'light'}`}>Clear Text</button>
             </div>
 
             <div className='container my-5'>
                 <h2>Your text Summary</h2>
                 <div>
-                    <p>Total Words: {text[text.length-1]===' ' || text.length===0 ? text.split(' ').length-1:text.split(' ').length} and Total characters:{text.length}</p>
+                    <p>Total Words: {text[text.length-1]===' ' || text.length===0 ? text.split(' ').length-1:text.split(' ').length}</p>
+                    <p>Total characters: {text.length}</p>
                     <p>Reading Time: {0.008 * text.split(" ").length} minutes</p>
                 </div>
 
-                <h3>Preview</h3>
-                <p>{text}</p>
+                <h3 style={{marginTop: '5%'}}>Preview</h3>
+                <p>{text.length>0?text:'Enter text to see preview here'}</p>
             </div>
         </>
     )
